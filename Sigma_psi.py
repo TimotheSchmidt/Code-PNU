a767850f0ea9666637587c54d4fa2e4ddfdc556b
TimotheSchmidt <91544054+TimotheSchmidt@users.noreply.github.com>
2023-01-18 10:12:25 +0100

    18 janvier

--- Sigma_psi.py
+++ Sigma_psi.py
@@ -5,58 +5,39 @@ from astropy.io import fits
 filename = "/Users/timotheschmidt/Documents/MINES/2A/Stage PNU/Simu/DATA_SIMS/Output00034_size0128_hyperCube.fits"
 file = fits.open(filename)
 
-    
-def _Q_tab(x,y):
+
+
+def Q_tab():
+    """returns the 2D array of all Q(x,y)"""
     p_0 = 1
-    Bx = file[4].data[x,y,:]
-    By = file[5].data[x,y,:]
-    Bz = file[6].data[x,y,:]
-    N = file[0].data[x,y,:]
+    Bx = file[4].data
+    By = file[5].data
+    Bz = file[6].data
+    N = file[0].data
     B  = Bx**2 + By**2 + Bz**2
     
-    vecQ = p_0 * N *(Bx**2 - By**2) / B 
-    Q = np.sum(vecQ)
+    Tab = p_0 * N *(Bx**2 - By**2) / B 
+    return np.sum(Tab, 2)
 
-    return Q
 
-def _U_tab(x,y):
+
+def U_tab():
     p_0 = 1
-    Bx = file[4].data[x,y,:]
-    By = file[5].data[x,y,:]
-    Bz = file[6].data[x,y,:]
-    N = file[0].data[x,y,:]
+    Bx = file[4].data
+    By = file[5].data
+    Bz = file[6].data
+    N = file[0].data
     B  = Bx**2 + By**2 + Bz**2
     
     
-    vecU = 2 * p_0 * N * Bx * By /B
-    U = np.sum(vecU)
+    Tab = 2 * p_0 * N * Bx * By /B
+    return np.sum(Tab, 2)
 
-    return U
 
-U_tab = np.vectorize(_U_tab)
-Q_tab = np.vectorize(_Q_tab)
-
-def _psi(Q,U):
-    return np.arctan(U/Q)
-
-psi = np.vectorize(_psi)
 
 def psi_tab():
-    #Q = Q_tab(np.arange(128), np.arange(128))        à essayer
-    #U  = U_tab(np.arange(128), np.arange(128))
-    
-    Q_Tab = np.zeros((128, 128))
-    U_Tab = np.zeros((128, 128))
-
-    for x in range(128):
-        for y in range(128):
-             Q_Tab[x, y] = Q_tab(x, y)
-             U_Tab[x, y] = U_tab(x, y)
-
-   
-    A = Q_Tab / U_Tab
-    tab = 0.5 * np.arctan(A)
-    return tab
+    U, Q = U_tab(), Q_tab()
+    return 0.5 * np.arctan2(U, Q)
 
 
 def sigma_psi_global():
